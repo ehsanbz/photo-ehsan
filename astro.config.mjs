b94142c667395cfeb020d0a275bd4369a905c974
ehsanbz <57605763+ehsanbz@users.noreply.github.com>
2026-09-23 20:05:18 +0200

    Use photos.ehsan.bz and add persistent main-site navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://photo.ehsan.bz',
+  site: 'https://photos.ehsan.bz',
   output: 'static',
   integrations: [sitemap()],
   trailingSlash: 'always',
